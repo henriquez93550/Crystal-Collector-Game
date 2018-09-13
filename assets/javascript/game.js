69c690fadb3c9918573d948ce670d1c316ed98d8
Erick Henriquez <henriquez93550@yahoo.com>
2018-09-13 12:09:10 -0500

    added crystal images and css hover

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,12 @@ var startGame = function () {
     //clears the crystals so they wont duplicate
     $(".crystals").empty();
 
+    var images = [
+        'https://vignette.wikia.nocookie.net/powerlisting/images/d/d3/2013-10-Blue-Crystal-Abstract.jpg/revision/latest?cb=20160227092023',
+        'http://www.scienceclarified.com/photos/crystal-3107.jpg',
+        'https://vignette.wikia.nocookie.net/mgefanon/images/6/69/Kinetic_Crystal.jpg/revision/latest?cb=20150409231719',
+        'https://www.djurmaxi.se/pub/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/9/2/927906.jpg'];
+
 //generates then writes the random result to HTML so it can show on the screen
 randomResult = Math.floor(Math.random() * 69 ) + 30;
 $("#result").html('Random Result: ' + randomResult);
@@ -29,19 +35,29 @@ for( var i = 0; i < 4; i++) {
             "class": 'crystal',
             "data-random": randomNum
         });
+    //adds crytal image to the crystal div so the user can see it    
+        crystal.css({
+            "background-image":"url('" + images[i] + "')",
+            //cover makes the picture to fit inside the div
+            "background-size":"cover"
+
+        });
+
         //Lets user see number values on crystal 
-        crystal.html(randomNum);
+        //crystal.html(randomNum);
 
     $(".crystals").append(crystal);
   }
+  $("#previous").html("Total Score: " + previous);
 }
 
+    
 startGame();
 
 var reset = function () {
 
 }
-//Event Delegation
+//Event Delegation 
 //allows player to click on crystal
 $(document).on('click', ".crystal", function () {
 
@@ -50,11 +66,14 @@ $(document).on('click', ".crystal", function () {
     //previous needs to be a number or else it is undefine. That is why var = 0 on the top
     previous += num;
 
+    //writes results on to the HTML
+    $("#previous").html("Crystals Collected: " + previous);
+
     console.log(previous);
     
     if(previous > randomResult) {
-        lost--;
-        $("#lost").html(lost);
+        lost++;
+        $("#lost").html("You Lost : " + lost);
         //reset counter to zero when person loses
         previous = 0;
 
@@ -62,7 +81,11 @@ $(document).on('click', ".crystal", function () {
     } 
     else if(previous === randomResult) {
         win++;
-        $("#win").html(win);
+        $("#win").html("Your Wins: " + win);
+       
+       
+
+        
         //resets counter to zero when user wins
         previous = 0;
 
